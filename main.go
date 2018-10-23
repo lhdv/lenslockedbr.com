08d67b7102c99353b2889940563df59ef40dd511
LH <lh@lhdv.net>
2018-10-23 13:40:58 -0300

    Changes to use httprouter instead of Gorilla Mux

--- main.go
+++ main.go
@@ -4,22 +4,22 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
-func contact(w http.ResponseWriter, r *http.Request) {
+func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "To get in touch, please send an email " +
                       "to <a href=\"mailto:support@lenslockedbr.com\">"+
                       "support@lenslockedbr.com</a>.")
 }
 
-func faq(w http.ResponseWriter, r *http.Request) {
+func faq(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>F.A.Q.</h1>" +
                       "<p>You'll find here some answers which could be " +
@@ -38,12 +38,12 @@ func notfound(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	r := mux.NewRouter()
+	r := httprouter.New()
 
-	r.NotFoundHandler = http.HandlerFunc(notfound)
+	r.NotFound = http.HandlerFunc(notfound)
 
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
+	r.GET("/", home)
+	r.GET("/contact", contact)
+	r.GET("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
