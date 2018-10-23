a3c4fc9892571047b519bdf94161e4d90d4893cf
LH <lh@lhdv.net>
2018-10-23 15:25:32 -0300

    Add a different dataset and a condition on the template

--- exp/main.go
+++ exp/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"os"
+	"time"
 )
 
 func main() {
@@ -12,11 +13,39 @@ func main() {
 	}
 
 	data := struct {
-		Name string
-		City string
-	} {"John Smith", "Bay Area"}
+		Name     string
+		City     string
+		Today    time.Time
+                Age      int
+                Weight   float64
+		Children map[string]int
+	} {"John Smith", 
+           "Bay Area", 
+           time.Now(),
+           33,
+           88.25,
+           map[string]int{"Jhonny":5, "Anne":10}}
 
 	err = t.Execute(os.Stdout, data)
+	if err != nil {
+		panic(err)
+	}
+
+	data1 := struct {
+		Name     string
+		City     string
+		Today    time.Time
+                Age      int
+                Weight   float64
+		Children map[string]int
+	} {"Mary Anne", 
+           "Texas", 
+           time.Now(),
+           23,
+           58.25,
+	   nil}
+
+	err = t.Execute(os.Stdout, data1)
 	if err != nil {
 		panic(err)
 	}
