beec9df61247735274ff67728661bedc65979f36
LH <lh@lhdv.net>
2018-10-23 17:17:58 -0300

    Add View type to put template logic together

--- main.go
+++ main.go
@@ -2,25 +2,27 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 
+	"lenslockedbr.com/views"
 	"github.com/gorilla/mux"
 )
 
-var homeTemplate *template.Template
-var contactTemplate *template.Template
+var homeView *views.View
+var contactView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := homeTemplate.Execute(w, nil); err != nil {
+	err := homeView.Template.Execute(w, nil)
+	if err != nil {
 		panic(err)
 	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := contactTemplate.Execute(w, nil); err != nil {
+	err := contactView.Template.Execute(w, nil);
+	if err != nil {
 		panic(err)
 	}
 }
@@ -44,19 +46,8 @@ func notfound(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	var err error
-
-	homeTemplate, err = template.ParseFiles("views/home.gohtml",
-                                            "views/layouts/footer.gohtml")
-	if err != nil {
-		panic(err)
-	}
-
-	contactTemplate, err = template.ParseFiles("views/contact.gohtml",
-                                            "views/layouts/footer.gohtml")
-	if err != nil {
-		panic(err)
-	}
+	homeView = views.NewView("views/home.gohtml")
+	contactView = views.NewView("views/contact.gohtml")
 
 	r := mux.NewRouter()
 
