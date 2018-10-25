3c71e140feb348ef60b4ed73ae41030d1fc41dfc
LH <lh@lhdv.net>
2018-10-25 17:46:21 -0300

    Add example code to create/add/query related data from Orders table to Users table

--- exp/main.go
+++ exp/main.go
@@ -20,8 +20,16 @@ const (
 
 type User struct {
 	gorm.Model
-	Name  string
-	Email string `gorm:"not null;unique_index"`
+	Name   string
+	Email  string `gorm:"not null;unique_index"`
+	Orders []Order
+}
+
+type Order struct {
+	gorm.Model
+	UserID      uint
+	Amount      int
+	Description string
 }
 
 func main() {
@@ -38,7 +46,7 @@ func main() {
 	fmt.Println("Successfully connected!")
 
 	db.LogMode(true)
-	db.AutoMigrate(&User{})
+	db.AutoMigrate(&User{}, &Order{})
 
 	// name, email := getInfo()
 	// u := &User{Name: name, Email: email}
@@ -64,6 +72,23 @@ func main() {
 	}
 	fmt.Println("Retrieved", len(users), " users")
 	fmt.Println(users)
+
+	// Add orders from users
+	// for _, u := range users {
+	// 	createOrder(db, u, int((u.ID*1000)+1), "Random description 01")
+	// 	createOrder(db, u, int((u.ID*1000)+9), "Random description 09")
+	// 	createOrder(db, u, int((u.ID*1000)+88), "Random description 88")
+	// }
+
+	u = &User{}
+	db.Preload("Orders").First(&u)
+	if db.Error != nil {
+		panic(db.Error)
+	}
+
+	fmt.Println("Email:", u.Email)
+	fmt.Println("Number of orders:", len(u.Orders))
+	fmt.Println("Orders:", u.Orders)
 }
 
 func getInfo() (name, email string) {
@@ -76,4 +101,15 @@ func getInfo() (name, email string) {
 	email = strings.TrimSpace(email)
 
 	return name, email
+}
+
+func createOrder(db *gorm.DB, user User, amount int, desc string) {
+	db.Create(&Order{
+		UserID:      user.ID,
+		Amount:      amount,
+		Description: desc,
+	})
+	if db.Error != nil {
+		panic(db.Error)
+	}
 }
