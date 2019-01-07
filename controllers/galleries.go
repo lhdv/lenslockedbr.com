e843e534238e45e717d963c4aa937ddaa2fb7051
LH <lh@lhdv.net>
2019-01-07 18:19:17 -0200

    14.8.3 - Create Update method for GalleryService interface updates the database. Uses galleriesValidator before update the given gallery

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -132,10 +132,17 @@ func (g *Galleries) Update(w http.ResponseWriter, r *http.Request) {
 	}
 
 	gallery.Title = form.Title
-	vd.Alert = &views.Alert {
-		Level: views.AlertLvlSuccess,
-		Message: "Gallery updated successfully!",
+
+	err = g.gs.Update(gallery)
+	if err != nil {
+		vd.SetAlert(err)
+	} else {
+		vd.Alert = &views.Alert {
+			Level: views.AlertLvlSuccess,
+			Message: "Gallery updated successfully!",
+		}
 	}
+
 	g.EditView.Render(w, vd)
 }
 
